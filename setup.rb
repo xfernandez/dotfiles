85e74839f6de759b857456bc93fae6cae5404a71
David Calavera <david.calavera@gmail.com>
2010-10-14 14:41:01 +0200

    add bashrc.d to setup

--- setup.rb
+++ setup.rb
@@ -6,6 +6,9 @@ def link(name, dest_name = name)
   ln_s File.join(dir, name), File.join(ENV['HOME'], dest_name)
 end
 
-link('.vim')
+['bashrc.d', '.vim'].each do |path|
+  link path
+end
+
 %w{vimrc gvimrc}.each {|path| link(".vim/#{path}", ".#{path}")}
 %w{backup swap}.each {|path| mkdir_p File.join(ENV['HOME'], '.vim', path)}
