f23a5294b61161e99210dfc3d428970bc9798cc4
David Calavera <david.calavera@gmail.com>
2010-12-12 18:05:17 +0100

    load submodules on setup

--- setup.rb
+++ setup.rb
@@ -7,9 +7,15 @@ def link(name, dest_name = name)
   ln_s(File.join(dir, name), dest) unless File.exist?(dest)
 end
 
+puts 'Loading submodules'
+`git submodule update --init`
+
+puts 'Creating symlinks'
 ['.dircolors', '.bash_profile', '.bashrc', 'bashrc.d', '.vim'].each do |path|
   link path
 end
 
 %w{vimrc gvimrc}.each {|path| link(".vim/#{path}", ".#{path}")}
 %w{backup swap}.each {|path| mkdir_p File.join(ENV['HOME'], '.vim', path)}
+
+puts 'Done.'
